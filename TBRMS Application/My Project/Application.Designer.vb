dfd19b999172026b2cd6c24daf2ef0f030c8d1ab
Justin Denchy <justindenchy@gmail.com>
2019-03-23 10:39:24 -0400

    a

--- TBRMS Application/My Project/Application.Designer.vb
+++ TBRMS Application/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TBRMS_Application.LoginScreen
+            Me.MainForm = Global.TBRMS_Application.LoginSplash
         End Sub
     End Class
 End Namespace
