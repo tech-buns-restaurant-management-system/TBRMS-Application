c3613ae89d7cdd784939c42726bd5e980877fd32
Justin Denchy <jd9655@ship.edu>
2019-03-24 14:56:59 -0400

    MenuItemCustomization Edit Item functionality

--- TBRMS Application/CustomerScreens/MenuItemCustomization.vb
+++ TBRMS Application/CustomerScreens/MenuItemCustomization.vb
@@ -8,10 +8,10 @@
         If CustomerOrderDashboard.blnEditItem = False Then
             CustomerOrderDashboard.lstOrderSummary.Items.Add(lblItem.Text)
         Else
-            'Concatenate customizations onto existing Order Line item
+            AddCustomizations()
         End If
-        CustomerOrderDashboard.Show()
 
+        CustomerOrderDashboard.Show()
         Me.Hide()
     End Sub
 
@@ -20,5 +20,43 @@
         pnlIng2.Visible = False
         pnlIng3.Visible = False
         pnlIng4.Visible = False
+    End Function
+
+    Function AddCustomizations()
+
+        If rdIng1None.Checked = True Then
+            Select Case lblIng1.Text
+                Case "Red Ale-Caramelized Onions"
+            End Select
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " N-" + lblIng1.Text.Remove(3, lblIng1.Text.Length - 3)
+        ElseIf rdIng1Light.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " L-" + lblIng1.Text.Remove(3, lblIng1.Text.Length - 3)
+        ElseIf rdIng1Extra.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " E-" + lblIng1.Text.Remove(3, lblIng1.Text.Length - 3)
+        End If
+
+        If rdIng2None.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " N-" + lblIng2.Text.Remove(3, lblIng2.Text.Length - 3)
+        ElseIf rdIng2Light.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " L-" + lblIng2.Text.Remove(3, lblIng2.Text.Length - 3)
+        ElseIf rdIng2Extra.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " E-" + lblIng2.Text.Remove(3, lblIng2.Text.Length - 3)
+        End If
+
+        If rdIng3None.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " N-" + lblIng3.Text.Remove(3, lblIng3.Text.Length - 3)
+        ElseIf rdIng3Light.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " L-" + lblIng3.Text.Remove(3, lblIng3.Text.Length - 3)
+        ElseIf rdIng3Extra.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " E-" + lblIng3.Text.Remove(3, lblIng3.Text.Length - 3)
+        End If
+
+        If rdIng4None.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " N-" + lblIng4.Text.Remove(3, lblIng4.Text.Length - 3)
+        ElseIf rdIng4Light.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " L-" + lblIng4.Text.Remove(3, lblIng4.Text.Length - 3)
+        ElseIf rdIng4Extra.Checked = True Then
+            CustomerOrderDashboard.lstOrderSummary.Items(CustomerOrderDashboard.lstOrderSummary.SelectedIndex) += " E-" + lblIng4.Text.Remove(3, lblIng4.Text.Length - 3)
+        End If
     End Function
 End Class
